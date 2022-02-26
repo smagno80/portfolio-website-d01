6c4e601ba5308f383e9ff469e01a943b0a234259
Pedro Camargo <sistemamagno@gmail.com>
2022-02-26 07:41:08 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+import './App.css';
+
+const App = (): ReactElement => {
+	return (
+		<div className='mx-auto h-full max-w-max'>
+			<h1>PORTFOLIO</h1>
+		</div>
+	);
+};
+
+export default App;
